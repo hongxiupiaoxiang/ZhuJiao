899f9400f19817e8e76817467ff71d4d440b463b
zfQiu <zfqiu@qh-speed.com>
2017-11-02 17:02:34 +0800

    asd

---
+++ RoleChat/RoleChat/Managers/QHSocketResponse.h
@@ -0,0 +1,13 @@
+//
+//  QHSocketResponse.h
+//  RoleChat
+//
+//  Created by zfqiu on 2017/11/1.
+//  Copyright © 2017年 QHCHAT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface QHSocketResponse : NSObject
+
+@end
---
+++ RoleChat/RoleChat/Managers/QHSocketResponse.m
@@ -0,0 +1,13 @@
+//
+//  QHSocketResponse.m
+//  RoleChat
+//
+//  Created by zfqiu on 2017/11/1.
+//  Copyright © 2017年 QHCHAT. All rights reserved.
+//
+
+#import "QHSocketResponse.h"
+
+@implementation QHSocketResponse
+
+@end
